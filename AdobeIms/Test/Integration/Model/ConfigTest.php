6ae902d103a753f796c6d3c232b6ae213fced11c
Vadim Malesh <51680850+engcom-Charlie@users.noreply.github.com>
2020-12-14 16:34:10 +0200

    updated namespace for integration test

--- AdobeIms/Test/Integration/Model/ConfigTest.php
+++ AdobeIms/Test/Integration/Model/ConfigTest.php
@@ -6,7 +6,7 @@
 
 declare(strict_types=1);
 
-namespace Magento\AdobeIms\Model;
+namespace Magento\AdobeIms\Test\Integration\Model;
 
 use Magento\TestFramework\Helper\Bootstrap;
 use PHPUnit\Framework\TestCase;
